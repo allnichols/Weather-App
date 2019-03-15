73ed39b6aa7cd63074b76d8daef19e932357d67a
allnichols <allenmaxnichols@gmail.com>
2019-03-14 22:28:25 -0500

    Typography Changes

--- src/components/Date/Date.js
+++ src/components/Date/Date.js
@@ -2,13 +2,10 @@ import React from 'react';
 import styles from './Date.module.css';
 
 var options = {
-                year: 'numeric',
-                month: 'long',
-                day: 'numeric',
-                hour: '2-digit',
-                minute: '2-digit',
-                hour12: true
-              };
+                weekday: 'long',
+                hour:'2-digit',
+                minute:'2-digit'
+                                };
 var prnDt = new Date().toLocaleTimeString('en-us', options);
 
 const date = () => (
